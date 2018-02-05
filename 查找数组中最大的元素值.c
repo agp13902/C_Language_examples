2bd4120b419a14a803206e6acf662b98e4c3d77a
张浩 <agp13@DESKTOP-TPPA773>
2018-02-05 21:00:40 +0800

    C 语言实例 - 查找数组中最大的元素值 C 语言实例 C 语言实例

---
+++ 查找数组中最大的元素值.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+// 题目要求
+// 查找数组中最大的元素值
+
+int main()
+{
+    int i,n;
+    float arr[10];
+
+    printf("输入元素个数为(0~100):");
+    scanf("%d",&n);
+    printf("\n");
+
+    // 接收用户的输入
+    for(i=0;i<n;++i)
+    {
+        printf("输入数字 %d:",i+1);
+        scanf("%f",&arr[i]);
+    }
+
+    // 循环,并将最大的元素存储到arr[0]
+    for(i=1;i<n;++i)
+    {
+        // 如果要查找最小值可以将 < 换成 >
+        if(arr[0]<arr[i])
+        {
+            arr[0]=arr[i];
+        }
+    }
+
+    printf("最大的元素为 = %.2f",arr[0]);
+
+    return 0;
+}
